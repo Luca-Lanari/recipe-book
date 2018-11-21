d381a8cac19c0e5c862b6a09267b3d7eac39129d
Luca <tomfordsawyer@gmail.com>
2018-11-21 16:29:19 +0100

    Add Recipes with dummy values

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Bread', 'This is the recipe for Bread ', 'https://c1.staticflickr.com/6/5166/5307865320_fcb349210c_b.jpg'),
+    new Recipe('Meatballs', 'Recipe for Meatballs', 'https://www.melarossa.it/wp-content/uploads/2014/06/ricetta-polpette-vegetariane.jpg'),
+    new Recipe('Fish and Chips', 'Fish and Chips recipe', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Modern_fish_and_chips_%288368723726%29.jpg/1024px-Modern_fish_and_chips_%288368723726%29.jpg')
+  ];
   
   constructor() { }
 
